a37a8372dcc200cda4e5e51a08bcc847934a05e7
Sim KyouYong <SKY@Sim-ui-MacBook-Pro.local>
2017-10-13 16:01:30 +0900

    1.Google Map 연동 완료

--- app/src/main/java/sky/foodstep/MainActivity.java
+++ app/src/main/java/sky/foodstep/MainActivity.java
@@ -1,13 +1,20 @@
 package sky.foodstep;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
+/*
+* AIzaSyC1bRHhlzxfHtELQLq1gqK2yV2XvfzXlgA
+* */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        //Intent intent4 = new Intent(MainActivity.this, DetailActivity.class);
+        //startActivity(intent4);
     }
 }
