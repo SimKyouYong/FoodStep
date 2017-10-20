e762929905892375262e4dd1c31cb5ef087aae58
Sim KyouYong <SKY@Sim-ui-MacBook-Pro.local>
2017-10-20 17:14:43 +0900

    1.끝 ..ㅡㅡ

--- app/src/main/java/sky/foodstep/common/DEFINE.java
+++ app/src/main/java/sky/foodstep/common/DEFINE.java
@@ -3,7 +3,8 @@ package sky.foodstep.common;
 public class DEFINE {
 
 
-    public static final String SERVER_URL       = "http://snap40.cafe24.com/FoodStep/php/";
+    //public static final String SERVER_URL       = "http://snap40.cafe24.com/FoodStep/php/";
+    public static final String SERVER_URL       = "http://kkm2117.cafe24.com/FoodStep/php/";
 
 
 
